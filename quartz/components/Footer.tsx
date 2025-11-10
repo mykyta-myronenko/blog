520947068cb74fee0805478e413ec32f90decaee
Mykyta Myronenko <mykmyro@gmail.com>
2025-11-10 21:49:41 +0000

    Quartz sync: Nov 10, 2025, 9:49 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -7,14 +7,16 @@ interface Options {
   links: Record<string, string>
 }
 
-
-/*
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
+        <p>
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+        </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
@@ -24,7 +26,7 @@ export default ((opts?: Options) => {
         </ul>
       </footer>
     )
-  }*/
+  }
 
   Footer.css = style
   return Footer
