8439749c3111f656fb8926945d08d2805cc78063
mykyta-myronenko <mykmyro@gmail.com>
2025-11-10 18:39:14 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,10 +15,10 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "en-UK",
+    baseUrl: "https://mykyta-myronenko.github.io/blog/",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "published",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
@@ -73,7 +73,10 @@ const config: QuartzConfig = {
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
-    filters: [Plugin.RemoveDrafts()],
+    filters: [
+      Plugin.RemoveDrafts(),
+      Plugin.ExplicitPublish(),
+      ],
     emitters: [
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
