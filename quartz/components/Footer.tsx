388e3e85e3d8173852c84962309f01b1677efc77
Mykyta Myronenko <mykmyro@gmail.com>
2025-11-11 01:21:38 +0000

    Quartz sync: Nov 11, 2025, 1:21 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,7 +13,7 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
+        <p class="visually-hidden">
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
