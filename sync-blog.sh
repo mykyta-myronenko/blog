78ef9415e19af2e0ef016cf5e8c9093fc068c727
Mykyta Myronenko <mykmyro@gmail.com>
2025-11-11 00:59:06 +0000

    Content sync: 2025-11-11 00:59:06

--- sync-blog.sh
+++ sync-blog.sh
@@ -7,11 +7,11 @@ git pull --rebase origin v4
 # Copy files from source to content folder  
 rsync -av --delete "/home/mykyta/Documents/Writing Machine/Thoughts/8 Interests/2 Writing/Blog/" "/home/mykyta/Documents/Blog/content/"  
   
-# Run Prettier
+# Run Prettier with absolute path
 npx prettier "/home/mykyta/Documents/Blog/content/"
-  
+
 # Stage changes  
-git add content/
+git add content/  
   
 # Commit and push  
 git commit -m "Content sync: $(date '+%Y-%m-%d %H:%M:%S')"  
